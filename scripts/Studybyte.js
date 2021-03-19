a8150e2b229647e544a509aad79c6eb922923345
Light-Lens <73633374+Light-Lens@users.noreply.github.com>
2021-03-19 13:06:46 +0530

    Optimize the search algorithm

--- scripts/Studybyte.js
+++ scripts/Studybyte.js
@@ -21,47 +21,28 @@ function Studybyte_search()
 function Studybyte_result()
 {
 	let Original_Query = localStorage.getItem("Original");
-	let Lowercase_Query = localStorage.getItem("Lowercase");
+	let Query = localStorage.getItem("Lowercase");
 	document.title = Original_Query + " - Studybyte";
 
-	const NameOfLinks = document.getElementById("LinkList").getElementsByTagName("ul");
-
-	var Link;
-	var Query = Lowercase_Query;
+	var CountHiddenLinks = 0;
 	const Chars = [" ", "-", "_"]
-	const Queries = [
-		["class5", "class-5"],
-		["class6", "class-6"],
-		["class7", "class-7"],
-		["class8", "class-8"],
-		["class9", "class-9"],
-		["class10", "class-10"],
-		["class11", "class-11"],
-		["class12", "class-12"],
-		["science", "science"],
-		["evs", "evs"],
-		["maths", "maths"],
-		["maths", "mathmatics"],
-		["english", "english"],
-		["hindi", "hindi"]
-	];
-
+	const NameOfLinks = document.getElementById("LinkList").getElementsByTagName("li");
 	for (var Check in Chars)
 	{
-		if (Lowercase_Query.includes(Chars[Check]) == false && Check == 2) break;
-		else if (Lowercase_Query.includes(Chars[Check])) Query = Query.split(Chars[Check]).join("");
+		if (Query.includes(Chars[Check])) Query = Query.split(Chars[Check]).join("-");
 	}
 
-	for (var Keys in Queries)
-	{
-		if (Query.includes(Queries[Keys][0])) Link = Queries[Keys][1];
-	}
-
-	if (Link == undefined) window.location = "Studybyte_ERROR.html";
+	if (Query == undefined) window.location = "Studybyte_ERROR.html";
 	for (var i = 0; i < NameOfLinks.length; i++)
 	{
-		if (NameOfLinks[i].id != Link) document.getElementById(NameOfLinks[i].id).style.display = "none";
+		if (NameOfLinks[i].id != Query)
+		{
+			document.getElementById(NameOfLinks[i].id).style.display = "none";
+			CountHiddenLinks++;
+		}
 	}
+
+	if (CountHiddenLinks == NameOfLinks.length) window.location = "Studybyte_ERROR.html";
 }
 
 function Showerror()
