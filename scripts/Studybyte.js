97d9751fd3aa085d88c9143b32b0acb1cfaba01a
Light-Lens <73633374+Light-Lens@users.noreply.github.com>
2021-03-19 14:36:46 +0530

    Add comments to the javascript file.

--- scripts/Studybyte.js
+++ scripts/Studybyte.js
@@ -1,13 +1,19 @@
+// This function will check whether Enter key is pressed or not?
 function IsEnter()
 {
-	// Checking whether Enter is pressed or not?
+	// If Enter key is pressed then call "Studybyte_search" function.
 	if (event.keyCode == 13) Studybyte_search();
 }
 
+// This function will try to send all of the data to "Studybyte_RESULTS" page.
 function Studybyte_search()
 {
+	// These variables will send data from the search box.
 	var Original_Data = document.getElementById("GetValue").value;
 	var Lowercase_Data = Original_Data.toLowerCase();
+
+	/*This piece of code will try to send all of the data to "Studybyte_RESULTS" page,
+	and if it's not possible then send to "Studybyte_ERROR" page.*/
 	try
 	{
 		localStorage.setItem("Original", Original_Data);
@@ -18,36 +24,54 @@ function Studybyte_search()
 	catch (err) { window.location = "Studybyte_ERROR.html"; }
 }
 
+// This is the main algorithm behind the Studybyte search engine.
 function Studybyte_result()
 {
+	// These variables will receive data from the search box.
 	let Original_Query = localStorage.getItem("Original");
 	let Query = localStorage.getItem("Lowercase");
-	document.title = Original_Query + " - Studybyte";
+	document.title = Original_Query + " - Studybyte"; // Change the title of the page.
 
+	// Global variables
 	var CountHiddenLinks = 0;
 	const Chars = [" ", "-", "_"]
 	const NameOfLinks = document.getElementById("LinkList").getElementsByTagName("li");
+
+	// This For Loop will check and replace all the Chars with "-".
 	for (var Check in Chars)
 	{
-		if (Query.includes(Chars[Check])) Query = Query.split(Chars[Check]).join("-");
+		if (Query.includes(Chars[Check])) Query = Query.split(Chars[Check]).join("-"); /*Replace all the Chars
+		with "-" to make it compare with the Database links in the future.*/
 	}
 
-	if (Query == undefined) window.location = "Studybyte_ERROR.html";
+	if (Query == undefined) window.location = "Studybyte_ERROR.html"; /*If Query is undefined then,
+	send to "Studybyte_ERROR" page.*/
+
+	/*This For Loop will check if there are links for the Query given,
+	and hide others*/
 	for (var i = 0; i < NameOfLinks.length; i++)
 	{
 		if (NameOfLinks[i].id != Query)
 		{
-			document.getElementById(NameOfLinks[i].id).style.display = "none";
-			CountHiddenLinks++;
+			document.getElementById(NameOfLinks[i].id).style.display = "none"; /*Hide all the links that
+			doesn't match with the Query.*/
+
+			CountHiddenLinks++; /*Do +1 in CountHiddenLinks variable every single time when it finds that
+			the current link doesn't match the Query.*/
 		}
 	}
 
-	if (CountHiddenLinks == NameOfLinks.length) window.location = "Studybyte_ERROR.html";
+	if (CountHiddenLinks == NameOfLinks.length) window.location = "Studybyte_ERROR.html"; /*This piece of code
+	will check whether the number of hidden links are equal to total number of links, and if yes then send to
+	"Studybyte_ERROR" page.*/
 }
 
+/*If you are redirected to "Studybyte_ERROR" page then this function will add the Original_Query to
+"Your search term was" and make it render.*/
 function Showerror()
 {
+	// Set the value of search term which was given in the search box.
 	let Original_Query = localStorage.getItem("Original");
 	document.getElementById("Searchterm").innerHTML = "Your search term was \"" + Original_Query + "\".";
-	document.title = Original_Query + " - Studybyte";
+	document.title = Original_Query + " - Studybyte"; // Change the title of the page.
 }
