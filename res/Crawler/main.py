c9ed3d8328c73f36343921a3b4f024c3c608005f
Light-Lens <73633374+Light-Lens@users.noreply.github.com>
2021-08-13 11:47:22 +0530

    Update the Crawler.

--- res/Crawler/main.py
+++ res/Crawler/main.py
@@ -11,10 +11,6 @@ print("Crawler.")
 Query = sys.argv[1].lower()
 LinksToExclude = ["facebook", "twitter", "reddit", "quora", "instagram", "amazon", "play.google", "apps.apple", "pinterest"]
 
-# Get the History of all crawled sites.
-if os.path.isfile("History.log"): Links = [open("History.log", "r").read()]
-else: Links = []
-
 # The main code.
 def ProcessJson(Filename):
     open("Pages.log", "w").write("") # Clear the Pages.log file.
@@ -26,34 +22,24 @@ def ProcessJson(Filename):
         for i in Content["results"][Data]:
             GenerateJson(i["title"], i["link"])
 
-    # Close the Json file.
+    # Close the Json file and Delete the Pages.json file.
     Json.close()
+    os.remove(f"{Filename}.json")
 
 def GenerateJson(Title, Link):
-    global Links # Get the global Links list.
+    if Link.endswith("/"): Link = Link[:-1] # Remove "/" from the ending of the link.
 
-    # Delete the Pages.json file.
-    os.remove("Pages.json")
-
-    # Remove "/" from the ending of the link,
-    # Check whether any link is repeating, and if yes, then don't write it to the memory.
-    if Link.endswith("/"): Link = Link[:-1]
-    if len(Links) > 0:
-        for i in Links:
-            if i == Link: return None
-
-    # If the link is provided by these sites then ignore them.
-    for LTE in LinksToExclude:
-        if LTE in Link.lower(): return None
+    # Ignore results from some websites.
+    for i in LinksToExclude:
+        if i in Link.lower(): return None
 
     # Write all the links and titles to a file in json manner.
-    Links.append(Link)
-    with open("History.log", "a", encoding="utf-8") as History: History.write(f"{Link}\n")
-    with open("Pages.log", "a", encoding="utf-8") as File:
-        File.write("{\n")
-        File.write(f"\t\"Title\": \"{Title}\",\n")
-        File.write(f"\t\"URL\": \"{Link}\"\n")
-        File.write("},\n")
+    Pages = open("Pages.log", "a", encoding="utf-8")
+    Pages.write("{\n")
+    Pages.write(f"\t\"Title\": \"{Title}\",\n")
+    Pages.write(f"\t\"URL\": \"{Link}\"\n")
+    Pages.write("},\n")
+    Pages.close()
 
 # Generate Files.
 os.system(f"python Crawler.py -e google,bing,duckduckgo -p 1 -i -q \"{Query}\" -o json,print -n Pages")
